4f27e8c472242532004c2a7768ce2d2120733475
Conor Hayes <ch1987@live.ie>
2015-02-05 21:30:12 +0000

    Added JAR for 0mq integration. Created context and publisher objects

--- src/AnnounceResult.java
+++ src/AnnounceResult.java
@@ -1,6 +1,11 @@
+import org.jeromq.ZMQ;
+import org.jeromq.ZMQ.*;
 
 public class AnnounceResult {
+    private Context context = ZMQ.context();
+    private Socket ackPublisher = context.socket(ZMQ.PUB);
+
     public static void main(String[] args) {
-        
+
     }
 }
